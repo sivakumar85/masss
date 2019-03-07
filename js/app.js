8c168413e486a68ea5d1412842894d95366e52e0
teja <teja85131@gmail.com>
2019-03-07 16:18:10 +0530

    added directive

--- js/app.js
+++ js/app.js
@@ -21,3 +21,26 @@ $routeProvider
 	templateUrl:"views/community-development.html"
 })
 });
+
+app.directive('loading',   ['$http' ,function ($http)
+ {
+     return {
+         restrict: 'A',
+         template: '<div id="divProcessing"><span class="std_loadingBackground"></span><div style="top: 30%; left: 40%;" class="std_modalContainer"><div class="std_tcenter"><img id="myAnimatedImage" alt="Processing.... Please wait!" src="images/ajax-loader.gif"><span class="std_processing">Processing... Please wait!</span></div></div></div>',
+         link: function (scope, elm, attrs)
+         {
+             scope.isLoading = function () {
+                 return $http.pendingRequests.length > 0;
+             };
+
+             scope.$watch(scope.isLoading, function (v)
+             {
+                 if(v){
+                     elm.show();
+                 }else{
+                     elm.hide();
+                 }
+             });
+         }
+     };
+ }]);
